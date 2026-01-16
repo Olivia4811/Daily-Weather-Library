f040da4fbff11d57272307dbaa673ed9672285c5
leah836 <lstacy26@students.stab.org>
2026-01-16 14:13:15 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 var url = "https://raw.githubusercontent.com/b-mcavoy/datasets/refs/heads/main/Science/Daily%20Weather.csv"
 var mainCondition = getColumn(url, 5)
+var conditionDesc = getColumn(url,6);
 var state = getColumn(url, 1)
 var city = getColumn (url, 2)
 var lowTemperature = getColumn (url, 8)
@@ -81,6 +82,62 @@ for (var i= 0; i < mainCondition.length; i++) {
     }
 
     console.log(getAvgHumidity("Las Vegas"))
+
+
+/*Returns the city with the highest temperature for a certain state
+@param chosenState {string} - the desired state
+@return greatestCity {string} - city with the highest temperature in the state
+*/
+function hottestCity(chosenState){
+var greatestTemp = 0;
+var greatestCity = "";
+    for (var i = 0; i < state.length; i++){
+        if (chosenState.toLowerCase().includes(state[i].toLowerCase()) && highTemperature[i] > greatestTemp){
+            greatestTemp = highTemperature[i];
+            greatestCity = city[i];
+        }
+    }
+    if(greatestCity == ""){
+    greatestCity = "State is not found"
+    }  
+ return greatestCity
+}
+console.log(hottestCity("mexico"));
+
+
+/*Returns weather condition descriptions and the frequencies for a main weather condition in a state
+@param chosenState {string} - desired state
+@param chosenCondition {stirng} - desired weather condition (clouds, snow, rain, clear)
+@return conditionStats {string} - each condition and the number of times it has been recorded
+*/
+function conditionBreakdown (chosenState, chosenCondition){
+var uniqueDesc = [];
+    for(i = 0; i < conditionDesc.length; i++){
+        if (chosenState.toLowerCase().includes(state[i].toLowerCase()) && chosenCondition.toLowerCase().includes(mainCondition[i].toLowerCase()) && !uniqueDesc.includes(conditionDesc[i])){
+            uniqueDesc.push(conditionDesc[i]);
+        }
+    }
+var conditionStats = "";
+    for (i = 0; i < uniqueDesc.length; i++){
+        var frequency = 0;
+            for (j = 0; j < conditionDesc.length; j++){
+                if(chosenState.toLowerCase().includes(state[j].toLowerCase()) && chosenCondition.toLowerCase().includes(mainCondition[j].toLowerCase())){
+                    if(conditionDesc[j] == uniqueDesc[i]){
+                        frequency ++;
+                    }
+            }
+        }
+        conditionStats += uniqueDesc[i] + ", "+frequency+"; ";
+    }
+        if(conditionStats == ""){
+    conditionStats = "State/Condition is not found"
+    }  
+    return conditionStats
+}
+console.log(conditionBreakdown("mexico", "sun"))
+
+
+
 
 
 
